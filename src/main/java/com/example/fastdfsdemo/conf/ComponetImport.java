11127e6f30867acd664ee3468982d31f8b031403
astupidcoder <15028562592@163.com>
2020-04-23 10:52:14 +0800

    init

---
+++ src/main/java/com/example/fastdfsdemo/conf/ComponetImport.java
@@ -0,0 +1,22 @@
+package com.example.fastdfsdemo.conf;
+
+import com.github.tobato.fastdfs.FdfsClientConfig;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.context.annotation.Import;
+import org.springframework.jmx.support.RegistrationPolicy;
+
+/**
+ * @author liuyj
+ * @Title: ComponetImport
+ * @create 2020-04-23 10:26
+ * @ProjectName fastdfs-demo
+ * @Description: TODO
+ */
+@Configuration
+@Import(FdfsClientConfig.class)
+// 解决jmx重复注册bean的问题
+@EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
+public class ComponetImport {
+    // 导入依赖组件
+}
